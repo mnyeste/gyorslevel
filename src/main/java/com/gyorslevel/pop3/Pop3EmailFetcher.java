07447011162480ee0c2f658d5361a469e44769a3
dave00 <dave00ster@gmail.com>
2012-08-25 16:50:25 +0200

    Fixes Issue #33, Closes Issue #33
    Code distinguish between html and text mails.

--- src/main/java/com/gyorslevel/pop3/Pop3EmailFetcher.java
+++ src/main/java/com/gyorslevel/pop3/Pop3EmailFetcher.java
@@ -16,9 +16,24 @@ import org.springframework.integration.mail.Pop3MailReceiver;
  * @author dave00
  */
 public class Pop3EmailFetcher {
-    
+
     private static Logger logger = Logger.getLogger(Pop3EmailFetcher.class);
     private String userName;
+
+    /**
+     * Represents the elements a parsed MimeMessage. Items contained can be type
+     * String or HTML.
+     */
+    static class MyBodyPart {
+
+        String text;
+        boolean html;
+
+        public MyBodyPart(String text, boolean html) {
+            this.text = text;
+            this.html = html;
+        }
+    }
 
     /**
      * Represents the tree structure of a parsed MimeMessage. Contains a list of
@@ -26,74 +41,79 @@ public class Pop3EmailFetcher {
      * overwritten by the filename where the image was actually saved.
      */
     static class BodyPartDOM {
-        
-        List<String> bodyParts = new ArrayList<String>();
+
+        List<MyBodyPart> bodyParts = new ArrayList<MyBodyPart>();
         // Content-ID:Filename
         Map<String, String> mappedFiles = new HashMap<String, String>();
-        
-        String processBodyParts() {
-            
+
+        String processBodyParts(boolean html) {
+
             StringBuilder builder = new StringBuilder();
-            
-            for (String bodyPart : bodyParts) {
-                Iterator<String> iterator = mappedFiles.keySet().iterator();
+
+            for (MyBodyPart bodyPart : bodyParts) {
                 
-                String part = bodyPart;
-                
-                while (iterator.hasNext()) {
-                    
-                    String contentId = iterator.next();
-                    if (bodyPart.contains(contentId)) {
-                        String fileName = mappedFiles.get(contentId);
-                        part = bodyPart.replaceAll(contentId, fileName);
-                    }
-                    
+                if (html != bodyPart.html) {
+                    continue;
                 }
                 
+                Iterator<String> iterator = mappedFiles.keySet().iterator();
+
+                String part = bodyPart.text;
+
+                while (iterator.hasNext()) {
+
+                    String contentId = iterator.next();
+                    if (bodyPart.text.contains(contentId)) {
+                        String fileName = mappedFiles.get(contentId);
+                        part = bodyPart.text.replaceAll(contentId, fileName);
+                    }
+
+                }
+
                 builder.append(part);
-                
+
             }
-            
+
             return builder.toString();
-            
+
         }
     }
-    
+
     public SimpleMessage[] fetchMessages(String host, String username, String password) throws MessagingException {
-        
+
         this.userName = username;
-        
+
         try {
-            
+
             final String domain = ConfigurationBean.getValue(ConfigurationBean.ConfigurationBeanKey.Domain, String.class);
-            
+
             final MailReceiver receiver = new Pop3MailReceiver(host, username + "@" + domain, password);
-            
+
             final Message[] messages = receiver.receive();
-            
+
             SimpleMessage[] subjects = new SimpleMessage[messages.length];
-            
+
             for (int i = 0; i < messages.length; i++) {
-                
+
                 final Message message = messages[i];
-                
+
                 final String subject = message.getSubject();
                 final Address address = message.getFrom()[0];
                 final InternetAddress internetAddress = (InternetAddress) address;
-                
+
                 // TODO: handle multiple 'from'
                 final String from = internetAddress.getAddress();
                 final Date sentDate = message.getSentDate();
                 final String sentDateStr = sentDate == null ? "" : sentDate.toString();
                 final String content = getContent(message);
                 subjects[i] = new SimpleMessage(Integer.toString(i + 1), content, subject, from, sentDateStr);
-                
+
                 logger.error("-*-*-*-* " + from);
-                
+
             }
-            
+
             return subjects;
-            
+
         } catch (MessagingException exception) {
             logger.error(exception);
             throw new RuntimeException(exception);
@@ -101,76 +121,85 @@ public class Pop3EmailFetcher {
             logger.error(exception);
             throw new RuntimeException(exception);
         }
-        
+
     }
-    
+
     String getContent(Message message) throws MessagingException, IOException {
-        
+
         logger.debug(message.getClass().getName());
-        
+
         if (message.isMimeType("text/plain") || message.isMimeType("text/html")) {
-            
+
             return (String) message.getContent();
-            
+
         } else if (message.isMimeType("multipart/related") || message.isMimeType("multipart/mixed") || message.isMimeType("multipart/alternative")) {
-            
+
             BodyPartDOM dom = collectBodyParts(message);
-            return dom.processBodyParts();
-            
+            return dom.processBodyParts(true);
+
         } else {
-            
+
             throw new UnsupportedOperationException("Not yet implemented:" + message.getContentType());
-            
+
         }
-        
+
     }
-    
+
     private BodyPartDOM collectBodyParts(Part part) throws IOException, MessagingException {
-        
+
         BodyPartDOM dom = new BodyPartDOM();
-        
+
         Object content = part.getContent();
-        
+
         logger.debug("handleBodyPart() - Datahandler: " + part.getDataHandler() + " FileName: " + part.getFileName() + " Description: " + part.getDescription() + " Headers: " + part.getAllHeaders());
-        
+
         if (content instanceof String) {
-            
+
             logger.debug("handleBodyPart() - Found String content");
-            dom.bodyParts.add((String) content);
-            
+
+            if (part.isMimeType("text/plain")) {
+
+                dom.bodyParts.add(new MyBodyPart((String) content, false));
+
+            } else if (part.isMimeType("text/html")) {
+
+                dom.bodyParts.add(new MyBodyPart((String) content, true));
+
+            }
+
         } else if (content instanceof Multipart) {
-            
+
             logger.debug("handleBodyPart() - Found nested Multipart content");
-            
+
             Multipart innerMultiPart = (Multipart) content;
             int count = innerMultiPart.getCount();
-            
+
             logger.debug("handleBodyPart() - Nested Multipart has [" + count + "] inner element");
-            
+
             for (int i = 0; i < count; i++) {
-                
+
                 BodyPart innerBodyPart = innerMultiPart.getBodyPart(i);
                 BodyPartDOM subDom = collectBodyParts(innerBodyPart);
                 dom.bodyParts.addAll(subDom.bodyParts);
                 dom.mappedFiles.putAll(subDom.mappedFiles);
-                
+
             }
-            
+
         } else if (content instanceof InputStream) {
-            
+
             final String contextFolder = ConfigurationBean.getValue(ConfigurationBean.ConfigurationBeanKey.ContextFolder, String.class);
             final String mailImagesFolder = "static";
             final String fileName = part.getFileName();
-            
+
             if (fileName == null) {
                 logger.error("handleBodyPart() - File name is null! Username: " + userName);
                 throw new RuntimeException("");
             }
-            
+
             final String contentId = part.getHeader("Content-ID")[0].replaceAll("<", "").replaceAll(">", "");
-            
+
             logger.debug("handleBodyPart() - Found nested InputStream content. FileName: " + fileName + " Content-ID: " + contentId + " Context Folder: " + contextFolder);
-            
+
             InputStream is = (InputStream) content;
             File f = new File(contextFolder + "/" + userName + "/" + fileName);
             OutputStream out = new FileOutputStream(f);
@@ -181,12 +210,12 @@ public class Pop3EmailFetcher {
             }
             out.close();
             is.close();
-            
+
             dom.mappedFiles.put("cid:" + contentId, mailImagesFolder + "/" + userName + "/" + fileName);
-            
+
         }
-        
+
         return dom;
-        
+
     }
 }
--- src/test/java/com/gyorslevel/pop3/BodyPartDOMTest.java
+++ src/test/java/com/gyorslevel/pop3/BodyPartDOMTest.java
@@ -1,5 +1,6 @@
 package com.gyorslevel.pop3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -15,14 +16,22 @@ public class BodyPartDOMTest {
         
         Pop3EmailFetcher.BodyPartDOM dom = new Pop3EmailFetcher.BodyPartDOM();
         
-        List<String> bodyParts = Arrays.asList(new String[]{"<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\" /></head><body text=\"#000000\" bgcolor=\"#FFFFFF\"><img src=\"cid:part1.08020903.00070500@localhost\" alt=\"\" /></body></html>"});
+        List<Pop3EmailFetcher.MyBodyPart> bodyParts = new ArrayList<Pop3EmailFetcher.MyBodyPart>();
+        List<String> bodyPartList = Arrays.asList(new String[]{"<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\" /></head><body text=\"#000000\" bgcolor=\"#FFFFFF\"><img src=\"cid:part1.08020903.00070500@localhost\" alt=\"\" /></body></html>"});
+        
+        for (String string : bodyPartList) {
+            bodyParts.add(new Pop3EmailFetcher.MyBodyPart(string, true));
+        }
+        
+        bodyParts.add(new Pop3EmailFetcher.MyBodyPart("ShouldNotBeIncluded", false));
+        
         dom.bodyParts = bodyParts;
         
         Map<String, String> mappedFiles = new HashMap<String, String>();
         mappedFiles.put("cid:part1.08020903.00070500@localhost", "/tmp/izeke.png");
         dom.mappedFiles = mappedFiles;
         
-        String result = dom.processBodyParts();
+        String result = dom.processBodyParts(true);
         Assert.assertEquals("<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\" /></head><body text=\"#000000\" bgcolor=\"#FFFFFF\"><img src=\"/tmp/izeke.png\" alt=\"\" /></body></html>", result);
         
     }
